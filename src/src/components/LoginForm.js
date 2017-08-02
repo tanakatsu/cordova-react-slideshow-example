eff986c926a3a181f4c2e595b4db53dc225e0e14
Tsuyoshi Tanaka <tanakatsu1080@gmail.com>
2017-08-03 04:04:48 +0900

    Centering login form

--- src/src/components/LoginForm.js
+++ src/src/components/LoginForm.js
@@ -21,12 +21,16 @@ class LoginForm extends React.Component {
 
   render() {
     return (
-      <div>
-        <div>
-          User ID <input type="text" ref={(input) => { this.inputUserId = input }} onChange={this.handleChange.bind(this)} />
-        </div>
-        <div>
-          Password <input type="password" ref={(input) => { this.inputPassword = input }} onChange={this.handleChange.bind(this)} />
+      <div style={{textAlign: 'center'}}>
+        <div style={{display: 'inline-block', textAlign: 'left'}}>
+          <div>
+            <span style={{display: 'inline-block', width: '80px'}}>User ID</span>
+            <input type="text" ref={(input) => { this.inputUserId = input }} onChange={this.handleChange.bind(this)} />
+          </div>
+          <div>
+            <span style={{display: 'inline-block', width: '80px'}}>Password</span>
+            <input type="password" ref={(input) => { this.inputPassword = input }} onChange={this.handleChange.bind(this)} />
+          </div>
         </div>
         <div>
           <input type="submit" value="Login" disabled={!this.state.formValid} onClick={() => { this.props.onSignIn(); this.props.history.push('/slick') }}/>
